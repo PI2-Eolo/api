a8462a13a55f3e8471d06af2202418dc8acfb54b
Matheus <matheusr.unb@gmail.com>
2022-04-22 04:56:03 -0300

    resolve cors

--- eolo_backend/eolo_api/settings.py
+++ eolo_backend/eolo_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r7s9*b^hj@_cr40w_tkabdqd6z$hqov@=#2#7#fv=0v^%=0+38
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
@@ -42,17 +42,23 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'data_app'
+    'data_app',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'eolo_api.urls'
